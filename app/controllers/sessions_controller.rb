1c1c595dec9f3651853e9b1777a5f55caf4a62fe
Breeza <cbasicok@gmail.com>
2015-08-10 17:26:06 +0800

    Add login page
    [Issue #1] https://github.com/GreenNerd/auth_server/issues/1
    
    https://trello.com/c/wMc10WXf/5--
    
    Add the login page and the 'new' method for the Sessions controller
    which will store informations given by Wifidog in the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,5 @@
 class SessionsController < ApplicationController
+  def new
+    session.merge!(params.slice(:gw_address, :gw_port, :gw_id, :url))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
 end
