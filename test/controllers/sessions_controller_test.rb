da6d89a943b260b8ee4c630a5b50173109ac885f
Breeza <cbasicok@gmail.com>
2015-08-10 17:15:42 +0800

    Test for Sessions controller
    [Issue #1] https://github.com/GreenNerd/auth_server/issues/1
    
    https://trello.com/c/wMc10WXf/5--
    
    Test for the Sessions controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get login page" do
+    get :new
+    assert_response :success, @response.body
+  end
+
+  test "should store information" do
+    get :new, gw_address: '199.199.199.199', gw_port: '2060', gw_id: '123', url: 'z.cn'
+    %w(gw_address gw_port gw_id url).each do |param|
+      assert_not_nil session[param.to_sym]
+    end
+  end
+
+  test "should redirect after login" do
+    session[:gw_address] = '199.199.199.199'
+    session[:gw_port] = '2060'
+    create(:user, name: 'name', password: '12345678')
+    post :create, user: { name: 'name', password: '12345678' }
+    assert_redirected_to 'http://199.199.199.199:2060/wifidog/auth'
+  end
 end
