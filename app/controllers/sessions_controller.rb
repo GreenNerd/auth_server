5ad59c543be20f6673d0470f28a3c5109e6a5ad1
Breeza <cbasicok@gmail.com>
2015-08-11 09:58:57 +0800

    Compete login
    [Issue #1] https://github.com/GreenNerd/auth_server/issues/1
    
    https://trello.com/c/wMc10WXf/5--
    
    Complete the create method of Sessions controller to allow users loggin
    in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,21 @@ class SessionsController < ApplicationController
   def new
     session.merge!(params.slice(:gw_address, :gw_port, :gw_id, :url))
   end
+
+  def create
+    @user = User.find_by(name: login_params[:name])
+
+    if @user && @user.authenticate(login_params[:password])
+      redirect_to wifi_auth_url(session[:gw_address], session[:gw_port])
+    else
+      flash[:now] = '用户名或密码错误，请重新登录'
+      render :new
+    end
+  end
+
+  private
+
+  def login_params
+    params.require(:user).permit(:name, :password)
+  end
 end
