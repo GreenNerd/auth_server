ee2a1418a3834d57d1b743d63d1bc2f33bdfdbf7
Breeza <cbasicok@gmail.com>
2015-08-12 17:32:24 +0800

    Test for Router controller
    [Issue #5] https://github.com/GreenNerd/auth_server/issues/5
    
    https://trello.com/c/0F6inHvA/9--
    
    Test for Router controller

--- test/controllers/router_controller_test.rb
+++ test/controllers/router_controller_test.rb
@@ -1,4 +1,29 @@
 require 'test_helper'
 
 class RouterControllerTest < ActionController::TestCase
+  test "should get error message page" do
+    get :message
+    assert_response :success, @response.body
+  end
+
+  test "should update router info" do
+    router = create(:router, gw_id: 'gwid')
+    ping_params = { gw_id: 'gwid',
+                    sys_uptime: 199512,
+                    sys_memfree: 951213,
+                    sys_load: '0.77',
+                    wifidog_uptime: 121319 }
+
+    get :ping, ping_params
+    router.reload
+
+    ping_params.each do |key,value|
+      assert_equal value, router.attributes[key.to_s]
+    end
+  end
+
+  test "should get success page" do
+    get :portal
+    assert_response :success, @response.body
+  end
 end
