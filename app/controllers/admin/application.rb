f34552c2f7ed054a9e56e4c3bad81787f70ad73a
Breeza <cbasicok@gmail.com>
2015-08-13 17:05:27 +0800

    Create Admin::Appliction controller
    [Issue #6] https://github.com/GreenNerd/auth_server/issues/6
    
    https://trello.com/c/51lnoFZd/10--
    
    Create Admin::Appliction controller with methods login_required and
    login_as

---
+++ app/controllers/admin/application.rb
@@ -0,0 +1,15 @@
+class Admin::ApplicationController < ApplicationController
+  before_action :login_required
+
+  private
+
+  def login_required
+    unless session[:admin_id].present?
+      redirect_to admin_login_url
+    end
+  end
+
+  def login_as(user)
+    session[:admin_id] = user.id
+  end
+end
