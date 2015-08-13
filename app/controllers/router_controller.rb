3e6c11bf74457eb6528631a1628720e9a8bc09e2
Breeza <cbasicok@gmail.com>
2015-08-13 09:48:03 +0800

    Fundamental functions of Router controller
    [Issue #5] https://github.com/GreenNerd/auth_server/issues/5
    
    https://trello.com/c/0F6inHvA/9--
    
    Fundamental functions of Router controller

--- app/controllers/router_controller.rb
+++ app/controllers/router_controller.rb
@@ -1,2 +1,15 @@
 class RouterController < ApplicationController
+  def message
+    flash[:now] = params[:message]
+  end
+
+  def ping
+    router = Router.find_by(gw_id: params[:gw_id])
+    router_params = params.slice(:sys_uptime, :sys_load, :sys_memfree, :wifidog_uptime).symbolize_keys
+    router.update(router_params)
+    render plain: 'Pong'
+  end
+
+  def portal
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
 
   get 'auth', to: 'user#auth', as: 'auth'
+
+  get 'gw_message', to: 'router#message', as: 'message'
+  get 'ping', to: 'router#ping', as: 'ping'
+  get 'portal', to: 'router#portal', as: 'portal'
 end
