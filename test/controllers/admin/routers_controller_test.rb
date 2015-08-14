a8d94acbb6368f31aff70e2619a1705b1a9f178b
Breeza <cbasicok@gmail.com>
2015-08-14 13:45:30 +0800

    Test for Admin::Routers controller
    [Issue #6] https://github.com/GreenNerd/auth_server/issues/6
    
    https://trello.com/c/51lnoFZd/10--
    
    Test for Admin::Routers controller

--- test/controllers/admin/routers_controller_test.rb
+++ test/controllers/admin/routers_controller_test.rb
@@ -1,4 +1,24 @@
 require 'test_helper'
 
 class Admin::RoutersControllerTest < ActionController::TestCase
+  def setup
+    @router = create(:router)
+    login_as create(:admin)
+  end
+
+  test "should get show page" do
+    get :show
+    assert_response :success, @response.body
+  end
+
+  test "should edit router" do
+    get :edit
+    assert_response :success, @response.body
+  end
+
+  test "should update router" do
+    xhr :patch, :update, router: { name: 'newrouter' }
+    @router.reload
+    assert_equal 'newrouter', @router.name
+  end
 end
