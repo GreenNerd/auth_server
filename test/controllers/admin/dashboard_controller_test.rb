2e492f0833f781af256600a6b9d1b1379940a935
Breeza <cbasicok@gmail.com>
2015-08-14 11:08:41 +0800

    Test for Dashboard controller
    [Issue #6] https://github.com/GreenNerd/auth_server/issues/6
    
    https://trello.com/c/51lnoFZd/10--
    
    Test for Dashboard controller

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class Admin::DashboardControllerTest < ActionController::TestCase
+  test "should redirect if no admin" do
+    get :show
+    assert_redirected_to admin_login_url
+  end
+
+  test "should get show page" do
+    login_as create(:admin)
+    get :show
+    assert_response :success, @response.body
+  end
 end
