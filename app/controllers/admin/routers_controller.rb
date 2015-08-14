b8e4031c23aef4dcd2d09e601660f560789d52ce
Breeza <cbasicok@gmail.com>
2015-08-14 13:49:59 +0800

    Complete Admin::Router resource
    [Issue #6] https://github.com/GreenNerd/auth_server/issues/6
    
    https://trello.com/c/51lnoFZd/10--
    
    Complete Admin::Router resource

--- app/controllers/admin/routers_controller.rb
+++ app/controllers/admin/routers_controller.rb
@@ -1,2 +1,27 @@
 class Admin::RoutersController < Admin::ApplicationController
+  before_action :find_router
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @router.update(router_params)
+      redirect_to admin_router_url
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def find_router
+    @router = Router.first
+  end
+
+  def router_params
+    params.require(:router).permit(:name)
+  end
 end
