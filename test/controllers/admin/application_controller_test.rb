cfaa23d886d7feddb6e5c116705e8b647ac57a78
Breeza <cbasicok@gmail.com>
2015-08-18 10:59:08 +0800

    Add a test detect find_router
    [Issue #6] https://github.com/GreenNerd/auth_server/issues/6
    
    https://trello.com/c/51lnoFZd/10--
    
    Add a test to detect the auto finding of current router after login as a
    admin

--- test/controllers/admin/application_controller_test.rb
+++ test/controllers/admin/application_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class Admin::ApplicationControllerTest < ActionController::TestCase
+  test "should find router" do
+    login_as create(:admin)
+    assert_not_nil find_router
+  end
 end
