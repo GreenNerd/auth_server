a567fe7511bef8b3e47dad2c97915afbf46206ec
Your Name <you@example.com>
2015-08-21 12:29:33 +0800

    NEW Finish Check Router's set Time

---
+++ db/migrate/20150821041957_add_check_time_to_routers.rb
@@ -0,0 +1,11 @@
+class AddCheckTimeToRouters < ActiveRecord::Migration
+  def change
+  add_column :routers, :start_minutes,  :integer, array: true, default: []
+    add_column :routers, :end_minutes,  :integer, array: true, default: []
+    add_column :routers, :start_day, :integer
+    add_column :routers, :end_day, :integer
+    add_column :routers, :start_month, :integer
+    add_column :routers, :end_month, :integer
+    add_column :routers, :time_count, :integer
+  end
+end
